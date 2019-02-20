d158bd0794d1c58978c72ea110bed5433b20ba7a
apoorv <apoorv7491@gmail.com>
2019-02-20 16:21:34 +0000

    initial commit, fetch total number of cases

---
+++ ik_insights.R
@@ -0,0 +1,23 @@
+
+library(magrittr)
+library(rvest)
+ik_url <- 'https://indiankanoon.org/search/?formInput=citedby%3A+1641007'
+
+ik_request <- httr::VERB(verb = 'GET',url = ik_url)
+ik_response <- httr::content(ik_request)
+
+# fetching insights from response
+
+
+# Total cases -------------------------------------------------------------
+total_data_points <-
+  ik_response  %>% 
+  html_nodes(xpath = "/html/body/div/div[3]/div[1]/b") %>% 
+  html_text() %>% 
+  stringr::str_replace_all(pattern = "[:digit:]+ - [:digit:]+ of ", "") %>% 
+  as.numeric()
+
+
+# DataFrame of cases ------------------------------------------------------
+
+
